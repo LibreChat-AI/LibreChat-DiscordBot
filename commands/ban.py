0c14d2a69b0e3e92fabc8c43fc1c2416c0b5449e
Fuegovic <32828263+fuegovic@users.noreply.github.com>
2023-11-03 13:14:58 -0400

    feat: ban 🔨 (#7)
    * feat: ban 🔨
    
    * feat: ban 🔨
    
    * feat: ban 🔨
    
    * feat: ban 🔨

---
+++ commands/ban.py
@@ -0,0 +1,46 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=import-error
+
+# 🔨 BAN USER
+
+from interactions import(
+    Extension,
+    Modal,
+    ModalContext,
+    ShortText,
+    SlashContext,
+    modal_callback,
+    slash_command,
+)
+from utils.utils import run_shell_command
+
+class Balance(Extension):
+    @slash_command(name="ban", description="🔨 Ban User")
+    async def balance_modal(self, ctx: SlashContext):
+        balance = Modal(
+            ShortText(
+                label="📧 User's email",
+                custom_id="email",
+                required=True,
+                min_length=6,
+                max_length=64,
+                placeholder="example@example.com",
+            ),
+            ShortText(
+                label="⌛ Duration in min. | example: 60*24 = 1 day",
+                custom_id="duration",
+                required=True,
+                placeholder="Duration of the ban in minutes.",
+            ),
+            title="🔨 Ban User",
+            custom_id="ban",
+        )
+        await ctx.send_modal(modal=balance)
+
+    @modal_callback("ban")
+    async def on_modal_answer(self, ctx: ModalContext, email: str, duration: str):
+        command = f"npm run ban-user {email} {duration}"
+        await ctx.send(f"{command}", ephemeral=True)
+        await run_shell_command(ctx, command)
--- commands/help.py
+++ commands/help.py
@@ -17,27 +17,27 @@ class Help(Extension):
     @slash_command(
             name="help",
             description="Show a list of available commands"
-            )
+        )
     async def help_command(self, ctx: SlashContext):
         embed = Embed(
             title="LibreChat Updater",
             description="Here is a list of available commands:",
             color=0x8000ff,
             url="https://github.com/Berry-13/LibreChat-DiscordBot"
-            )
+        )
         # Add fields for each command
         embed.add_field(
             name="/ping",
             value=
             "☎️ Ping the bot \n"
             "- ping the bot, returns the latency in milliseconds"
-            )
+        )
         embed.add_field(
             name="/librechat",
             value=
             "🌐 Explore LibreChat URLs \n"
             "- Quick access to LibreChat **GitHub**, **Documentation**, **Discord** and **YouTube**"
-            )
+        )
         embed.add_field(
             name="/path",
             value=
@@ -47,7 +47,7 @@ class Help(Extension):
             "Example: `C:\LibreChat`\n"
             "- or the relative path format (relative to the bot folder).\n"
             "Example: `../LibreChat`"
-            )
+        )
         embed.add_field(
             name="/env",
             value=
@@ -56,7 +56,7 @@ class Help(Extension):
             "- **Upload**: Upload a new `.env` file to replace the current one (You must include the `.env` file when calling **/env**) This command will also backup your current `.env` file before replacing it with the uploaded file \n"
             "- **Restore**: Restore the `.env` file backup and __**delete**__ the current .env file \n"
             "- **Example**: Download the `.env.example` from the LibreChat folder"
-            )
+        )
         embed.add_field(
             name="/docker",
             value=
@@ -65,7 +65,7 @@ class Help(Extension):
             "- **Stop**: Stop the docker container\n"
             "- **Update**: Update LibreChat (this may take several minutes).\n"
             "- **Status**: Display the status of all Docker containers."
-            )
+        )
         embed.add_field(
             name="/docker-single",
             value=
@@ -74,7 +74,7 @@ class Help(Extension):
             "- **Stop**: Stop the docker container\n"
             "- **Update**: Update LibreChat (this may take several minutes).\n"
             "- **Status**: Display the status of all Docker containers."
-            )
+        )
         embed.add_field(
             name="/local",
             value=
@@ -82,7 +82,7 @@ class Help(Extension):
             "- **Start**: start LibreChat\n"
             "- **Stop**: Stop LibreChat\n"
             "- **Update**: Update LibreChat (this may take several minutes)."
-            )
+        )
         embed.add_field(
             name="/balance",
             value=
@@ -90,7 +90,13 @@ class Help(Extension):
             "- user email is required\n"
             "- Set the following .env variable to enable this `CHECK_BALANCE=true`\n"
             "- 1000 credits = $0.001"
-            )
+        )
+        embed.add_field(
+            name="/ban",
+            value=
+            "🔨 Ban a user\n"
+            "- user email is required\n"
+        )
         embed.add_field(
             name="---",
             value="\n"
