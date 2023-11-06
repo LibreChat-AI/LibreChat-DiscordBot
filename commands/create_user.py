6e3c3378a0d46d775304be365d4a458505416e35
Fuegovic <32828263+fuegovic@users.noreply.github.com>
2023-11-06 12:18:14 -0500

    feat: Create User 🙆 (#8)
    * feat: Add User 🙆
    
    * feat: Create User 🙆

---
+++ commands/create_user.py
@@ -0,0 +1,68 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=import-error
+
+# 🙆 ADD USER
+
+from interactions import(
+    Extension,
+    Modal,
+    ModalContext,
+    ShortText,
+    SlashContext,
+    modal_callback,
+    slash_command,
+)
+from utils.utils import run_shell_command
+
+class CreateUser(Extension):
+    @slash_command(name="create-user", description="🙆 Create User")
+    async def adduser_modal(self, ctx: SlashContext):
+        balance = Modal(
+            ShortText(
+                label="📧 Email",
+                custom_id="email",
+                required=True,
+                min_length=6,
+                max_length=64,
+                placeholder="example@example.com",
+            ),
+            ShortText(
+                label="🖊️ Name",
+                custom_id="name",
+                min_length=3,
+                required=True,
+                placeholder="Name",
+            ),
+            ShortText(
+                label="✏️ Username",
+                custom_id="username",
+                min_length=2,
+                required=True,
+                placeholder="Username",
+            ),
+            ShortText(
+                label="🔐 Password",
+                custom_id="password",
+                required=True,
+                min_length=8,
+                max_length=128,
+                placeholder="1234567890",
+            ),
+            title="🙆 Create User",
+            custom_id="create-user",
+        )
+        await ctx.send_modal(modal=balance)
+
+    @modal_callback("create-user")
+    async def on_modal_answer(
+        self, ctx: ModalContext,
+        email: str,
+        name: str,
+        username: str,
+        password: str
+        ):
+        command = f'npm run create-user "{email}" "{name}" "{username}" "{password}"'
+        await ctx.send(f"{command}", ephemeral=True)
+        await run_shell_command(ctx, command)
